e07568dce9af5b9acaea951784c17508f15677a0
Carlos Perez <ca.perez.barr@gmail.com>
2021-12-02 13:07:57 -0600

    Update: Fizz was added to function.

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,21 +1,25 @@
+import java.util.Scanner;
+
+import javax.sql.rowset.CachedRowSet;
+
 public class FizzBuzz {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0) {
+            System.out.print("Fizz");
+        }
+
+
+        
     }
 }
 
-
-
-
-
-
-// public class Main {
-
-//     public static void FizzBuzz(Strings[] args) {
-        
-//         System.out.print("Test");
-        
-//     }
-    
-// }   
