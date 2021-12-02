2175960ea34ac30c195875a60d917938cd9450f6
Carlos Perez <ca.perez.barr@gmail.com>
2021-12-02 12:53:24 -0600

    Initial Commit: FizzBuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,21 @@
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+
+
+
+
+
+
+// public class Main {
+
+//     public static void FizzBuzz(Strings[] args) {
+        
+//         System.out.print("Test");
+        
+//     }
+    
+// }   
